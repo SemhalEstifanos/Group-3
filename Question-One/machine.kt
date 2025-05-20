176cc5f8653c823e919c3096654c74e0fed8adb5
Student <Adeday.adedayhaftu@gmail.com>
2025-05-20 15:18:13 +0300

    added kotlin file for kotlin question

---
+++ Question-One/machine.kt
@@ -0,0 +1,28 @@
+Design a maintenance sceduling system for industrial equipment used in manufacturing. 
+This system must optimize the timing of maintenance to minimize downtime and ensure machine 
+operates at peak efficiency .Consider the operational demands of the plant ,the availability
+ of maintenance personnel and parts and the historical data of equipment wears and failures.
+
+ 
+data class Maintain(var name: String, var demand: String, var historicalData: Int, var personnel: Int)
+fun maintenancePriority(machines: List<Maintain>) {
+    for (item in machines) {
+        if (item.demand == "High" && item.historicalData <= 3 && item.personnel <= 4) {
+            println("${item.name}:Takes high priority")
+        }
+        else if (item.demand == "Medium" && item.historicalData 4 to 7 && item.personnel 5..7){
+            println("${item.name}:Takes average priority")
+        }
+        else {
+            println("${item.name}:Takes low priority")
+        }
+    }
+}
+
+fun main() {
+    val machines = listOf(
+        Maintain("Machine A", "Low", 2, 4),
+        Maintain("Machine B", "High", 3, 3),
+    )
+    maintenancePriority(machines)
+}
