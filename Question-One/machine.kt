99f4ca7cdd59067b55e7b351c6d74b12ebb6d045
Student <Adeday.adedayhaftu@gmail.com>
2025-05-20 15:41:18 +0300

    edited kotline question

--- Question-One/machine.kt
+++ Question-One/machine.kt
@@ -3,11 +3,11 @@ This system must optimize the timing of maintenance to minimize downtime and ens
 operates at peak efficiency .Consider the operational demands of the plant ,the availability
  of maintenance personnel and parts and the historical data of equipment wears and failures.
 
- 
-data class Maintain(var name: String, var demand: String, var historicalData: Int, var personnel: Int)
+
+data class Maintain(var name: String, var demand: String, var historicalData: Int, var personnel: Int, var part: String)
 fun maintenancePriority(machines: List<Maintain>) {
     for (item in machines) {
-        if (item.demand == "High" && item.historicalData <= 3 && item.personnel <= 4) {
+        if (item.demand == "High" && item.historicalData <= 3 && item.personnel <= 4 && item.part == "Engine") {
             println("${item.name}:Takes high priority")
         }
         else if (item.demand == "Medium" && item.historicalData 4 to 7 && item.personnel 5..7){
@@ -21,8 +21,8 @@ fun maintenancePriority(machines: List<Maintain>) {
 
 fun main() {
     val machines = listOf(
-        Maintain("Machine A", "Low", 2, 4),
-        Maintain("Machine B", "High", 3, 3),
+        Maintain("Machine A", "Low", 2, 4, "Gear"),
+        Maintain("Machine B", "High", 3, 3, "Engine"),
     )
     maintenancePriority(machines)
 }
